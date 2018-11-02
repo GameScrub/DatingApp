9ebace73eaf81382b16a06ee766684cf1b586078
Rafael Artigas <gamescrubs@gmail.com>
2018-11-02 09:03:07 -0700

    Added error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,18 +19,18 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('logged in');
     }, error => {
-      console.log('Error logging in')
+      console.log(error);
     });
   }
 
-  loggedIn(){
+  loggedIn() {
     const token = localStorage.getItem('token');
 
     // returns true if it has data false if null
     return !!token;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
     console.log('logged out');
   }
